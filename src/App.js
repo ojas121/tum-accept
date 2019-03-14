f6f92feea76a6b27d3ab1d1f60c5fab3a1b4354d
Ojas Mandlekar <ojas.m@hotmail.com>
2019-03-14 09:22:51 +0100

    Starting to code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleTable from './GradesTable.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scoreNoExtra: 0,
+      bonus: 0,
+    };
+  }
+
+  changeBaseScore = () => {};
+  changeBonus = () => {};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SimpleTable/>
       </div>
     );
   }
