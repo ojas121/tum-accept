086c7e971b4e4ed277b17595203356fe8402ab90
Ojas Mandlekar <ojas.m@hotmail.com>
2019-03-14 09:48:55 +0100

    Selects created

--- src/GradesTable.js
+++ src/GradesTable.js
@@ -20,50 +20,55 @@ const styles = theme => ({
 });
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(subject, y1s1, y1s2, y2s1, y2s2, final) {
     id += 1;
-    return { id, name, calories, fat, carbs, protein };
+    return { id, subject, y1s1, y1s2, y2s1, y2s2, final };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Language 1', 6, 6, 5, 6, 6),
+    createData('Language 2', 6, 6, 5, 6, 6),
+    createData('Humanities', 6, 6, 5, 6, 6),
+    createData('Science', 6, 6, 5, 6, 6),
+    createData('Mathematics', 6, 6, 5, 6, 6),
+    createData('Group 6', 6, 6, 5, 6, 6),
+
 ];
 
-function SimpleTable(props) {
-    const { classes } = props;
+class SimpleTable extends React.Component {
+    render() {
+        const { classes } = this.props;
 
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat (g)</TableCell>
-                        <TableCell align="right">Carbs (g)</TableCell>
-                        <TableCell align="right">Protein (g)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => (
-                        <TableRow key={row.id}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Subject</TableCell>
+                            <TableCell align="right">IB1 Sem 1</TableCell>
+                            <TableCell align="right">IB1 Sem 2</TableCell>
+                            <TableCell align="right">IB2 Sem 1</TableCell>
+                            <TableCell align="right">IB2 Sem 2</TableCell>
+                            <TableCell align="right">Final Score</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 SimpleTable.propTypes = {
---
+++ src/Selects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+class GradeSelector extends React.Component {
+
+}
+
+class SubjectSelector extends React.Component {
+
+}
+
+export default {GradeSelector, SubjectSelector};
