68b1383372098fe79914c800e5f0b9e6af158fab
Ojas Mandlekar <ojas.m@hotmail.com>
2019-03-21 20:04:37 +0100

    Deploying

---
+++ docs/precache-manifest.d5bb0eea0be26ecd90eef1ba9a1b9fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1df9a082d0bca5aa7d1",
+    "url": "/tum-accept/static/css/main.2d58623c.chunk.css"
+  },
+  {
+    "revision": "b1df9a082d0bca5aa7d1",
+    "url": "/tum-accept/static/js/main.3ab1fc4b.chunk.js"
+  },
+  {
+    "revision": "ebec95732f75c0032eaa",
+    "url": "/tum-accept/static/js/runtime~main.dca98343.js"
+  },
+  {
+    "revision": "1a24933bbd2e4911f9a6",
+    "url": "/tum-accept/static/js/2.bfb45431.chunk.js"
+  },
+  {
+    "revision": "15cf677f7cf88e8386974776828efd41",
+    "url": "/tum-accept/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85371157c775b904903437f3f303db73.js"
+  "/tum-accept/precache-manifest.d5bb0eea0be26ecd90eef1ba9a1b9fc4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tum-accept/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
